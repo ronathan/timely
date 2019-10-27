9e9b21f6537384c24b5f173adcd5bdd86945e835
Ronald Victorino Reyes <ronaldvictorino@gmail.com>
2019-10-27 18:16:04 +0000

    Timer stop and start is working

--- app/src/main/java/com/ronathan/timely/ui/TimerActivity.kt
+++ app/src/main/java/com/ronathan/timely/ui/TimerActivity.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.PersistableBundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 import com.ronathan.timely.R
 import com.ronathan.utils.Stopwatch
 
@@ -21,15 +22,43 @@ class TimerActivity : AppCompatActivity() {
 
     // region UI Properties
     @BindView(R.id.timer) lateinit var timerTextView: TextView
+
+    @OnClick(R.id.startButton)
+    fun onStartClicked() {
+        stopwatch.start()
+        isRunning = true
+        handler.post(stopwatchTimer)
+    }
+
+    @OnClick(R.id.stopButton)
+    fun onStopClicked() {
+        isRunning = false
+        stopwatch.stop()
+    }
     // endregion
 
     // region Properties
     private val stopwatch = Stopwatch()
+    private val handler = Handler()
+    private var isRunning = false
+    private val stopwatchTimer = object : Runnable {
+        override fun run() {
+            if (isRunning) {
+                setTimerText(stopwatch.elapsedTime)
+                handler.postDelayed(this, 100)
+            }
+        }
+    }
     // endregion
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_timer)
+        ButterKnife.bind(this)
+    }
+
+    private fun setTimerText(time: Long) {
+        timerTextView.text = time.toString()
     }
 }
